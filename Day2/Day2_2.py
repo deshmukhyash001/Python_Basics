2b8633b3b020ae227b4ce9e4245d69ddb770b118
deshmukhyash001 <130223783+deshmukhyash001@users.noreply.github.com>
2025-06-17 09:36:21 +0530

    Add files via upload

---
+++ Day2/Day2_2.py
@@ -0,0 +1,12 @@
+def main():
+    
+    print("Enter no1 : ")
+    a = int(input())
+
+    print("Enter no2 : ")
+    b = int(input())
+
+    print("addition is ",a+b)
+    
+if __name__ == '__main__':
+    main()
