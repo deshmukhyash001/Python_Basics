11021ac0f3d86afd44c977d3d7bdf8f6929d513a
deshmukhyash001 <130223783+deshmukhyash001@users.noreply.github.com>
2025-06-17 09:29:25 +0530

    Add files via upload

---
+++ Day2/Day2_1.py
@@ -0,0 +1,21 @@
+def main():
+# Logic 1
+
+    a = 2
+    b = 3
+    c = None
+    
+    c = a
+    a = b 
+    b = c
+    
+    # Number Swaped
+    
+# Logic 2
+
+    a = 2
+    b = 3
+    a,b = b,a
+
+if __name__ == '__main__':
+    main()
